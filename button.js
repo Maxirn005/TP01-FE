1a3bc080054192561ee4a66dac9221c2d229c324
maximiliano waimann <maxiwrn@hotmail.com>
2022-09-29 19:26:20 -0300

    mucho

---
+++ button.js
@@ -0,0 +1,16 @@
+// Botón:
+let mybutton = document.getElementById("botontop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
